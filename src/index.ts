38d103c83d36275bd906fe55c9567be81e061fb8
Ahmed Fasih <wuzzyview@gmail.com>
2020-08-08 19:45:20 -0400

    clicks on morphemes will fetch dict-hits-per-line

--- src/index.ts
+++ src/index.ts
@@ -10,10 +10,27 @@ ReactDOM.render(
 );
 */
 
-{
+(async function main() {
+  const HASH_PREFIX = 'hash-';
   const lines: Element[] = Array.from(document.querySelectorAll('line'));
+  for (const node of lines) {
+    node.addEventListener('click', async (e) => {
+      const target: Element|null = e.target as any;
+      const parent: Element|null|undefined = target?.parentElement;
+      const lineId = parent?.id;
+      if (lineId && lineId.startsWith(HASH_PREFIX)) {
+        const hash = lineId.slice(HASH_PREFIX.length);
+        const response = await fetch(`/dict-hits-per-line/line-${hash}.json`);
+        if (response.ok) {
+          const payload = await response.json();
+          console.log(payload);
+        }
+      }
+      console.log({e})
+    });
+  }
   console.log(lines);
-}
+})();
 
 // Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
 // Learn more: https://www.snowpack.dev/#hot-module-replacement
