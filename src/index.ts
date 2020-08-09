fedb79843fc851c3f4f73c4505f83c0a8dc9d789
Ahmed Fasih <wuzzyview@gmail.com>
2020-08-08 21:06:19 -0400

    search for dictionary hits on click

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import './index.css';
 
+import {isRight} from 'fp-ts/lib/Either';
+import * as t from 'io-ts';
+
 /*
 import App from './App';
 import React, {createElement as ce} from 'react';
@@ -10,26 +13,72 @@ ReactDOM.render(
 );
 */
 
-(async function main() {
-  const HASH_PREFIX = 'hash-';
-  const lines: Element[] = Array.from(document.querySelectorAll('line'));
-  for (const node of lines) {
-    node.addEventListener('click', async (e) => {
-      const target: Element|null = e.target as any;
-      const parent: Element|null|undefined = target?.parentElement;
-      const lineId = parent?.id;
-      if (lineId && lineId.startsWith(HASH_PREFIX)) {
-        const hash = lineId.slice(HASH_PREFIX.length);
-        const response = await fetch(`/dict-hits-per-line/line-${hash}.json`);
-        if (response.ok) {
-          const payload = await response.json();
-          console.log(payload);
+const Morpheme = t.type({
+  literal: t.string,
+  pronunciation: t.string,
+  lemmaReading: t.string,
+  lemma: t.string,
+  partOfSpeech: t.array(t.string),
+  inflectionType: t.union([t.array(t.string), t.null]),
+  inflection: t.union([t.array(t.string), t.null]),
+});
+
+const ScoreHit = t.type({
+  wordId: t.string,
+  score: t.number,
+  search: t.string,
+  run: t.string,
+  summary: t.string,
+});
+
+const Dict = t.type({
+  line: t.string,
+  bunsetsus: t.array(t.array(Morpheme)),
+  dictHits: t.array(t.array(t.array(ScoreHit))),
+});
+
+const HASH_PREFIX = 'hash-';
+
+async function clickHandler(e: Event): Promise<void> {
+  const target: Element|null = e.target as any;
+  const parent: Element|null|undefined = target?.parentElement;
+  const lineId = parent?.id;
+  if (lineId && lineId.startsWith(HASH_PREFIX) && parent && target?.nodeName === 'MORPHEME') {
+    const children = parent.querySelectorAll('morpheme');
+    let morphemeIdx = 0;
+    for (; morphemeIdx < children.length; morphemeIdx++) {
+      if (children[morphemeIdx] === target) { break; }
+    }
+    if (morphemeIdx >= children.length) {
+      // couldn't find morpheme?
+      console.error('Error finding morpheme in line');
+      return;
+    }
+    const hash = lineId.slice(HASH_PREFIX.length);
+    const response = await fetch(`/dict-hits-per-line/line-${hash}.json`);
+    if (response.ok) {
+      const decoded = Dict.decode(await response.json());
+      if (isRight(decoded)) {
+        // we could do basic checks here like ensuring we have as many morphemes in `bunsetsus` and `dictHits` as
+        // `children` above
+
+        const hits = decoded.right.dictHits[morphemeIdx];
+        if (hits && hits.length) {
+          // found some hits
+          console.log(hits);
         }
+      } else {
+        console.error(`Error decoding dictionary results sidecar file`);
       }
-      console.log({e})
-    });
+    } else {
+      console.error('Error requesting dictionary results sidecar file', response.statusText);
+    }
   }
-  console.log(lines);
+}
+
+(function main() {
+  const lines: Element[] = Array.from(document.querySelectorAll('line'));
+  for (const node of lines) { node.addEventListener('click', clickHandler); }
 })();
 
 // Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
