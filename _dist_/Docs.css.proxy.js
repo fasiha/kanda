622fa61148f638c9fd5fef6a9ad643b1eac14286
Ahmed Fasih <wuzzyview@gmail.com>
2020-08-27 21:00:56 -0400

    style textarea

--- _dist_/Docs.css.proxy.js
+++ _dist_/Docs.css.proxy.js
@@ -29,4 +29,9 @@ const code=`.container {
 
 .annotated-entry {
   font-weight: bold;
+}
+
+.doc textarea{
+  width: 100%;
+  height: 33vh;
 }`,styleEl=document.createElement("style"),codeEl=document.createTextNode(code);styleEl.type="text/css",styleEl.appendChild(codeEl),document.head.appendChild(styleEl);
