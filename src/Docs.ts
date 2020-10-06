b56ee99a1f969a3b0062ae451c525ea01f2459b7
Ahmed Fasih <wuzzyview@gmail.com>
2020-10-05 20:46:47 -0400

    minor improvement in hash-to-hex

--- src/Docs.ts
+++ src/Docs.ts
@@ -802,11 +802,9 @@ Helper functions
 ************/
 // https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest
 async function digestMessage(message: string, algorithm: string) {
-  const msgUint8 = new TextEncoder().encode(message);                           // encode as (utf-8) Uint8Array
-  const hashBuffer = await crypto.subtle.digest(algorithm, msgUint8);           // hash the message
-  const hashArray = Array.from(new Uint8Array(hashBuffer));                     // convert buffer to byte array
-  const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join(''); // convert bytes to hex string
-  return hashHex;
+  const msgUint8 = new TextEncoder().encode(message);                 // encode as (utf-8) Uint8Array
+  const hashBuffer = await crypto.subtle.digest(algorithm, msgUint8); // hash the message
+  return Array.from(new Uint8Array(hashBuffer), b => b.toString(16).padStart(2, '0')).join('');
 }
 
 function runToString(run: string|ContextCloze): string {
