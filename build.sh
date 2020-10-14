40be4ffd23a0e52d240fb575ef14099e5d81ed07
Ahmed Fasih <wuzzyview@gmail.com>
2020-10-13 21:24:24 -0400

    clear gh-pages worktree before copying dist

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 # using git worktrees http://blog.jenkster.com/2016/02/git-for-static-sites.html
 
-rm build && npx snowpack build && cp -pr build/* ../curtiz-annotate-sidecar-web-gh-pages
+rm ../curtiz-annotate-sidecar-web-gh-pages/* && rm build && npx snowpack build && cp -pr build/* ../curtiz-annotate-sidecar-web-gh-pages
 
